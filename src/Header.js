26bd9622d93c8853b14e5644c79b2bfa383fba3f
Amber Stumpf <amacbain1@msn.com>
2018-10-04 13:30:29 -0700

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+
+  render(){
+      return(
+        <div>
+          <h1>
+            Neighborhood Map
+          </h1>
+        </div>
+
+      );
+  }
+}
+
+export default Header;
---
+++ src/Top-Sidebar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class TopSidebar extends Component {
+
+  render(){
+      return(
+        <div>
+          <h2>
+            Places of Interest
+          </h2>
+        </div>
+      );
+  }
+
+}
+
+export default TopSidebar;
