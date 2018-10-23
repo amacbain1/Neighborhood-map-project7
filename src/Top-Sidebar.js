1adcd440a29ef0b241e5e2587c24e79b119b1c6e
Amber Stumpf <amacbain1@msn.com>
2018-10-23 12:54:21 -0700

    Sidebar list now filtering

--- src/Top-Sidebar.js
+++ src/Top-Sidebar.js
@@ -21,7 +21,7 @@ class TopSidebar extends Component {
   }
 
   filterLocations = (query) => {
-    this.props.localArts.forEach(marker => {
+  //  this.props.localArts.forEach(marker => {
   //    marker.name.toLowerCase().includes(query.toLowerCase()) == true ?
   //      marker.setVisible(true) :
   //      marker.setVisible(false)
@@ -31,12 +31,12 @@ class TopSidebar extends Component {
     //  }else{
     //    this.props.localArts.marker
     //  }
-      console.log(marker)
+  //    console.log(marker)
 
 
     this.setState({ query })
 
-    })
+  //  })
   }
 
   showOnClick = e => {
@@ -47,19 +47,21 @@ class TopSidebar extends Component {
   render(){
 
     let filterLocations = this.props.localArts.filter( (localArt) => {
-      return localArt.venue.name.indexOf(this.state.query) !== -1
+      return localArt.venue.name.toLowerCase().indexOf(this.state.query.toLowerCase()) !== -1
     })
       return(
       <div className='options'>
         <h2 className='top-sidebar'>Art and Theater</h2>
         <div className='drop-down'>
-          <i className='far fa-times-circle' style= {{padding: '15px' }} type='button' value='Hide options' onClick={this.showOnClick}/>
+
+          <i className='fas fa-arrow-alt-circle-up' style= {{padding: '15px' }} type='button' value='Hide options' onClick={this.showOnClick}/>
           <input className='see-options' type='text' placeholder='Search' value={this.state.query} onChange={(event) => { this.filterLocations(event.target.value) }}/>
         </div>
+
         <div className='show-locations'>
           {this.state.showLocations ? (
           <ul className='list-museums'>
-            {this.props.localArts.map((localArt) => {
+            {filterLocations.map((localArt) => {
               return(
                 <li key={localArt.venue.id} className='list-item'>
 
@@ -75,10 +77,8 @@ class TopSidebar extends Component {
         </div>
 
       </div>
-
       );
   }
-
 }
 
 export default TopSidebar;
