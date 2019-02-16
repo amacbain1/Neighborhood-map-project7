83ec971fe73180dc26d26a5863421632a264c595
Amber Stumpf <aiyemacbain@gmail.com>
2019-02-15 16:00:36 -0800

    Updates

---
+++ precache-manifest.853df536c14dc17b726fb16c070dfc30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee5f480470df811fcf4e",
+    "url": "/amacbain1/Neighborhood-map-project7.git/static/js/runtime~main.ee5f4804.js"
+  },
+  {
+    "revision": "ff4507a2e3f7637b3b00",
+    "url": "/amacbain1/Neighborhood-map-project7.git/static/js/main.ff4507a2.chunk.js"
+  },
+  {
+    "revision": "1c247da633bcdb48f6ec",
+    "url": "/amacbain1/Neighborhood-map-project7.git/static/js/1.1c247da6.chunk.js"
+  },
+  {
+    "revision": "ff4507a2e3f7637b3b00",
+    "url": "/amacbain1/Neighborhood-map-project7.git/static/css/main.f9190253.chunk.css"
+  },
+  {
+    "revision": "4c59bc9710ce3a2db031c965cf383b52",
+    "url": "/amacbain1/Neighborhood-map-project7.git/index.html"
+  }
+];
